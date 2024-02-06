51b1bb29858cab0f954339952e0bf8cc4f604e78
guru1112001 <gbhatia266@gmail.com>
2024-02-07 01:09:45 +0530

    build custom authentication backend

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -19,12 +19,26 @@ class UserRegisterationForm(forms.ModelForm):
         if cd['password']!=cd['password2']:
             raise forms.ValidationError("password dont match")
         return cd['password']
+    
+    def clean_email(self):
+        data=self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('email already exists')
+        return data
         
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model=User
         fields=['first_name','last_name','email']
+    
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id)\
+                         .filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
