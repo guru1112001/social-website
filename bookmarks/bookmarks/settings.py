5ca07af693528922bf2a5758c75efbc00805b155
guru1112001 <gbhatia266@gmail.com>
2024-02-08 23:56:01 +0530

    added social auth pipeline

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -146,3 +146,15 @@ AUTHENTICATION_BACKENDS = [
  'social_core.backends.google.GoogleOAuth2',
  ]
 SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI='https://mysite.com:8000/social_auth/complete/google-oauth2/'
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+ ]
